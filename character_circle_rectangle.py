624d5e89cce35dd8779bd6876fe186573541ad79
wonju <jwwon00@naver.com>
2023-09-21 09:58:12 +0900

    원과 사각형 무한루프 완성

--- character_circle_rectangle.py
+++ character_circle_rectangle.py
@@ -5,4 +5,16 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def run_circle():
+    print("circle")
+    pass
+
+def run_rectangle():
+    print("rectangle")
+    pass
+
+while True:
+    run_circle()
+    run_rectangle()
+
 close_canvas()
