239b7bda8c80a4f2b315b9d0c076c25e9dbb2930
wonju <jwwon00@naver.com>
2023-09-21 10:35:44 +0900

    화면 그리기 함수로 만들기

--- character_circle_rectangle.py
+++ character_circle_rectangle.py
@@ -1,9 +1,14 @@
 from pico2d import *
 import math
 open_canvas()
-
+#에자일 방법론
 grass = load_image('grass.png')
 character = load_image('character.png')
+
+def render_frame(x,y):
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(x,y)
 
 def run_circle():
     print("circle")
@@ -15,24 +20,23 @@ def run_circle():
         x = cx + r * math.cos(math.radians(deg))
         y = cy + r * math.sin(math.radians(deg))
 
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
+        render_frame(x,y)
         delay(0.01)
 
 def run_rectangle():
     print("rectangle")
 
-    for x in range(50,750,10):
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,90)
+    for x in range(50,750+1,10):
+        render_frame(x,90)
         delay(0.01)
 
-    pass
+    for x in range(750,50-1,-10):
+        render_frame(x,90)
+        delay(0.01)
+
 
 while True:
-    #run_circle()
+    run_circle()
     run_rectangle()
 
     break
